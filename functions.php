e1bf7881f794705f79069dae229372e6393727a0
Dalphs <simon-dalby@hotmail.com>
2020-08-05 15:48:14 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+
+//Indlæser level fra .txt fil med vilkårligt navn og konverterer til 4x4 array
+function convertLevel($name) {
+    if ($file = fopen($name . ".txt", "r")) {
+        
+        $data = fgets($file);
+        $numbers = explode("-", $data);
+        
+        $counter = 0;
+        $board;
+        for ($i = 0; $i < 4; $i++) {
+            for($j = 0; $j < 4; $j++) {
+                $board[$i][$j] = $numbers[$counter];
+                $counter++;
+            }
+        }
+        return $board;
+    }else {
+        return -1;
+    }
+}
+
+function parseBoard($board) {
+    $htmlString = 
+            '<div id="board">' .
+            '  <div id="grid">';
+    for ($i = 0; $i < 4; $i++) {
+        for($j = 0; $j < 4; $j++) {
+            $htmlString .= '<div class="tile" id="t' . $board[$i][$j] . '">' . 
+                    (($board[$i][$j] == 16) ? '-' : $board[$i][$j]) . '</div>';
+            
+        }
+    }
+    $htmlString .= 
+            "  </div>" .
+            "</div>";
+    return $htmlString;
+}
+
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <?php
+        include "functions.php";
+        //Opgave 1: Filformatet er en txt hvor tallene skirves på første linje
+        // og alle tal adskilles af "-". Derudover remgår den blanke som 16.
+        // Tallene skrives på række i stedet for 4x4 format.
+        //Eksempel:
+        // 1  2  3  4
+        // 5  6  7  8
+        // 9  10 11 12
+        // 13 14 15
+        //konverteret til filformatet vil det se ud som følgende:
+        //1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16
+       
+        $new = convertLevel("solvable");
+        $html = parseBoard($new);
+        echo $html;
+        ?>
+    </body>
+</html>
